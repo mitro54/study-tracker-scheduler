9d3ce5cb88a0054cfd3692f39c98799d44c904e0
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-06-21 13:53:11 +0300

    Additions to logic
    Added a new selection to scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -11,7 +11,8 @@ def scheduler():
 
                 if user_input == "y":
                     print("Uploading schedule to Google Calendar...")
-                    # If ok ... print "success!""
+                    # If ok ... print "success!"
+                    break
 
                 elif user_input == "n" or user_input == "b":
                     break
@@ -19,7 +20,42 @@ def scheduler():
                     sys.exit("Quitting...")
 
         elif user_input == "2":
-            print("2")
+            while True:
+                user_input = input("Please provide your new schedule a length in days: ").lower()
+
+                # Testing if input is int
+                try:
+                    int(user_input)
+
+                    while True:
+                        user_input = input("Keep any data from previous schedule? y/n: ").lower()
+
+                        if user_input == "y":
+                            print("Previous schedule gets printed here")
+
+                            while True:
+                                user_input = input("What to keep? Format: day/hourstart, day/hourend (write k when done): ")
+
+                                if user_input == "k":
+                                    # Then erase everything else from the JSON file and start generating new schedule, will create separate functions for this task
+                                    break
+
+                        elif user_input == "n":
+                            # Start generating schedule, will create separate functions for this task once main frame is done
+                            print("Generating new schedule")
+                            break
+
+                        break
+                    break
+
+                # If input is not int, do not raise error
+                except ValueError:
+                    if user_input == "b":
+                        break
+                    elif user_input == "q":
+                        sys.exit("Quitting...")
+
+
         elif user_input == "3":
             print("3")
         elif user_input == "4":
