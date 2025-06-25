b047b1be41a65f81ed963db6764835ec18fff478
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-06-25 19:31:01 +0300

    Add logic to helpers.py

--- helpers.py
+++ helpers.py
@@ -7,11 +7,16 @@ def printer(filename: str):
 
 
 def add_to_json(filename: str, temp_list: list):
+    dict_list = []
     temp_dict = {}
-    for idx in temp_list:
-        test = {idx: "0"}
-        temp_dict.update(test)
-        # IN PROGRESS, DOES NOT WORK YET
-
-    with open(f"{filename}", "a") as storage:
-        json.dump(temp_dict, storage)
+    with open(f"{filename}", "r+") as storage:
+        storage.seek(0,2)
+        if storage.tell() == 0:
+            for idx in temp_list:
+                test = {idx: "0"}
+                temp_dict.update(test)
+                # IN PROGRESS, DOES NOT WORK YET
+            dict_list.append(temp_dict)
+            json.dump(dict_list, storage)
+        else:
+            print("not empty")
