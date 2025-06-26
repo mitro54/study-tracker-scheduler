76e1725708f42276ee42ce8e77ca04c3cea1875c
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-06-26 19:45:39 +0300

    add logic to helpers.py - prints now correctly the lines in tracker.json - if file is empty, handles it

--- helpers.py
+++ helpers.py
@@ -2,21 +2,27 @@
 import json
 
 def printer(filename: str):
-    with open(f"{filename}", "a") as storage:
-        print(storage)
+    with open(f"{filename}", "a+") as storage:
 
+        storage.seek(0,2)
+        if storage.tell() == 0:
+            print("Tracking has no data yet, add something!")
+
+        else:
+            storage.seek(0)
+            data = json.load(storage)
+            for obj in data:
+                for key, value in obj.items():
+                    print(f"{key}: {value}")
 
 def add_to_json(filename: str, temp_list: list):
     dict_list = []
-    temp_dict = {}
     with open(f"{filename}", "r+") as storage:
         storage.seek(0,2)
         if storage.tell() == 0:
             for idx in temp_list:
-                test = {idx: "0"}
-                temp_dict.update(test)
-                # IN PROGRESS, DOES NOT WORK YET
-            dict_list.append(temp_dict)
+                dict_list.append({idx: "0"})
+
             json.dump(dict_list, storage)
         else:
             print("not empty")
